b952b55eecc2e2c6b0aff550ea57b4b74c4817eb
robertmadril <robertmmadril@gmail.com>
2019-02-28 15:39:43 -0700

    add js pseudocode

---
+++ assets/javascript/app.js
@@ -0,0 +1,16 @@
+var topics = ["Hamburgers", "Pizza", "Mac+And+Cheese", "Pasta", "Tacos", "More Food Here"]
+
+
+/*
+
+Pseudocode:
+
+On page ready, buttons will populate into btn-container with information from topics array.
+User can add free text search and button will be added/move on to display
+When user clicks on button, that button topic will call to the GIF API with specific topic, amount
+Returned GIF API call will append 10 relevant gif's to gif-container, all static.
+If user clicks on gif's, they will animate.
+If user clicks on another gif or the same gif, animation will stop.
+User clicks new button or searches and 10 new gifs will appear in gif-container.
+
+*/
