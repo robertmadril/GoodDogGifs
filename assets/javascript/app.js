7e033c3c167deed85ec7a83861cbeea84a95d452
robertmadril <robertmmadril@gmail.com>
2019-03-01 17:00:23 -0700

    add js function to render buttons

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,11 +1,10 @@
-var topics = ["Hamburgers", "Pizza", "Mac+And+Cheese", "Pasta", "Tacos", "More Food Here"]
+var topics = ["Hamburgers", "Pizza", "Mac and Cheese", "Pasta", "Tacos", "More Food Here"]
 
 
 /*
 
 Pseudocode:
 
-On page ready, buttons will populate into btn-container with information from topics array.
 User can add free text search and button will be added/move on to display
 When user clicks on button, that button topic will call to the GIF API with specific topic, amount
 Returned GIF API call will append 10 relevant gif's to gif-container, all static.
@@ -14,3 +13,15 @@ If user clicks on another gif or the same gif, animation will stop.
 User clicks new button or searches and 10 new gifs will appear in gif-container.
 
 */
+
+function renderButtons() {
+    for (i=0; i<topics.length; i++) {
+        var newBtn = $("<button>")
+        newBtn.addClass("topic");
+        newBtn.attr("data-value", topics[i]);
+        newBtn.text(topics[i]);
+        $("#btn-container").append(newBtn);
+    }
+}
+
+renderButtons();
